44e6fd7b42a30dabc6eb72ff3bfb2e4276d12342
Nad2207 <nadiagunjana@gmail.com>
2023-06-30 16:46:36 +0200

    create mineprojekter.js

---
+++ javascript/mineprojekter.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var slides = document.querySelectorAll(".slide");
+    var currentSlide = 0;
+    var slideInterval = setInterval(nextSlide, 3000);
+  
+    function nextSlide() {
+      slides[currentSlide].style.display = "none";
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].style.display = "block";
+    }
+  });
